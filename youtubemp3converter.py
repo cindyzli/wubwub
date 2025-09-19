0ac2ebf0aceb724a4cb002352ea8496497eb9452
Cindy Li <63604424+cindyzli@users.noreply.github.com>
2025-09-19 19:42:21 -0400

    Add files via upload

---
+++ youtubemp3converter.py
@@ -0,0 +1,24 @@
+import yt_dlp
+from pydub import AudioSegment
+import os
+
+def youtube_to_mp3(url, output_folder="downloads"):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'{output_folder}/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+url = "https://www.youtube.com/watch?v=fI569nw0YUQ&ab_channel=CherylLynnVEVO" 
+youtube_to_mp3(url)
