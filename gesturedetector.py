ae194e35a485eb4fab4dac6903b553695305068b
elise-yz <elise.zyh@gmail.com>
2025-09-20 10:44:54 -0400

    Add nightcore and sound bites

--- gesturedetector.py
+++ gesturedetector.py
@@ -8,6 +8,9 @@ detector = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detecti
 
 # Global counter variables
 fingers = 0
+nightcore_frames = 0
+not_nightcore_frames = 0
+nightcore = 0
 dj_hands_ids = []
 bass_boost = 50 # starting bass boost
 volume = 50 # starting volume
@@ -17,16 +20,58 @@ prev_vol_y = None # vertical position of left hand
 # Initialize video capture
 cap = cv2.VideoCapture(0)
 
-# Detect flat palm facing down
+# Detect fist
 def isFist(hand):
     if not hand or "lmList" not in hand:
         return False
     fingerup = detector.fingersUp(hand)
-    return fingerup == [0, 1, 0, 0, 0]
+    return fingerup == [0, 0, 0, 0, 0]
+
+# Function to process frame and count fingers raised
+def isSoundBite(hand):
+    global fingers
+    fingerup = detector.fingersUp(hand)   
+    
+    if fingerup == [0, 1, 0, 0, 0] and fingers != 1: 
+        fingers = 1
+        return True
+    elif fingerup == [0, 1, 1, 0, 0] and fingers != 2: 
+        fingers = 2
+        return True
+    elif fingerup == [0, 1, 1, 1, 0] and fingers != 3: 
+        fingers = 3
+        return True
+    elif fingerup == [0, 1, 1, 1, 1] and fingers != 4: 
+        fingers = 4
+        return True
+    return False
+
+# Function to check if left hand is making rock sign
+def isNightCore(hand):
+    landmarks = hand['lmList']
+
+    thumb_tip = landmarks[4]   # Thumb tip (x, y)
+    index_tip = landmarks[8]   # Index tip (x, y)
+    middle_tip = landmarks[12] # Middle tip (x, y)
+    ring_tip = landmarks[16]   # Ring tip (x, y)
+    pinky_tip = landmarks[20]  # Pinky tip (x, y)
+    
+    # Check if thumb and pinky are extended
+    thumb_extended = thumb_tip[1] < landmarks[3][1]
+    index_extended = index_tip[1] < landmarks[9][1]
+    pinky_extended = pinky_tip[1] < landmarks[17][1]
+
+    # Check if other fingers are curled
+    middle_curl = middle_tip[1] > landmarks[10][1]
+    ring_curl = ring_tip[1] > landmarks[14][1]
+
+    if thumb_extended and pinky_extended and index_extended and middle_curl and ring_curl:
+        return True
+    return False
 
 # Function to process the frame and generate JSON commands
 def process_frame_and_generate_command(img):
-    global dj_hands_ids, bass_boost, volume, prev_bass_y, prev_vol_y
+    global dj_hands_ids, bass_boost, volume, prev_bass_y, prev_vol_y, fingers, nightcore_frames, not_nightcore_frames, nightcore
 
     # Hand detection
     hands, new_img = detector.findHands(img, draw=True, flipType=True)
@@ -69,6 +114,10 @@ def process_frame_and_generate_command(img):
                             return {"action": "adjust_bass", "bass": bass_boost}, new_img
                     prev_bass_y = y  # update every frame
 
+                elif isSoundBite(hand):
+                    print("sound bite: " + str(fingers))
+                    return {"action": "sound_bite", "number": fingers}, new_img
+                
             if h in dj_hands_ids and hand["type"] == "Left":
                 _, y, _, _ = hand["bbox"]
                 if isFist(hand):
@@ -82,6 +131,18 @@ def process_frame_and_generate_command(img):
                             print("volume:", volume)
                             return {"action": "adjust_vol", "volume": volume}, new_img
                     prev_vol_y = y  # update every frame
+                elif isNightCore(hand):
+                    nightcore_frames += 1
+                    if nightcore_frames >= 5 and not_nightcore_frames >= 5:
+                        nightcore_frames = 0
+                        not_nightcore_frames = 0
+                        nightcore = 1 - nightcore
+                        print("nightcore:", nightcore)
+                        return {"action": "toggle_nightcore", "state": nightcore}, new_img
+                else:
+                    if not_nightcore_frames < 5:
+                        not_nightcore_frames += 1
+                    nightcore_frames = 0
 
     return {"action": "none"}, new_img
 
